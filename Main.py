f6ee84a9c5ef9e6e249c63ca60ceddf5d265eb61
chokoo7 <adanzamudio777@gmail.com>
2023-07-20 10:48:39 -0600

    Nuevo Archivo

--- Main.py
+++ Main.py
@@ -2,3 +2,9 @@ print("Hola mundo")
 nombre = str(input("Ingresa tu nombre "))
 edad = int(input("Ingresa su edad "))
 print("mucho gusto ",nombre,"tienes ",edad," años")
+
+if(edad >= 18):
+    print("Eres mayor de edad")
+
+else:
+    print("Eres menor de edad")
